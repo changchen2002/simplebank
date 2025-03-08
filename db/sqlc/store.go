d6a46d6139705facd3b003f197fd79444d9b839d
changchen2002 <changchen2002@outlook.com>
2025-03-07 21:20:58 -0800

    implement RESTful HTTP API with Gin and load config with Viper

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,21 +6,21 @@ import (
 	"fmt"
 )
 
-//Store provides all functions to execute db queries and transactions
+// Store provides all functions to execute db queries and transactions
 type Store struct {
 	*Queries
 	db *sql.DB
 }
 
-//NewStore creates a new Store
+// NewStore creates a new Store
 func NewStore(db *sql.DB) *Store {
-	return &Store {
-		db:			db,
-		Queries:	New(db),
+	return &Store{
+		db:      db,
+		Queries: New(db),
 	}
 }
 
-//execTx executes a function within a database transaction
+// execTx executes a function within a database transaction
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -46,41 +46,41 @@ type TransferTxParams struct {
 }
 
 type TransferTxResult struct {
-	Transfer 	Transfer	`json:"transfer"`
-	FromAccount Account		`json:"from_account"`
-	ToAccount   Account		`json:"to_account"`
-	FromEntry 	Entry		`json:"from_entry"`
-	ToEntry   	Entry		`json:"to_entry"`
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
-//TransferTx performs a money transfer from one account to the other
-//It creates the transfer, add account entries, and update accounts' balance within a database transaction
+// TransferTx performs a money transfer from one account to the other
+// It creates the transfer, add account entries, and update accounts' balance within a database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
-	err := store.execTx(ctx, func(q * Queries) error {
+	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
-			FromAccountID: 	arg.FromAccountID,
-			ToAccountID: 	arg.ToAccountID,
-			Amount: arg.Amount,
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
 		})
 		if err != nil {
 			return err
 		}
 
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: 	arg.FromAccountID,
-			Amount: 	-arg.Amount,
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
 		})
 		if err != nil {
 			return err
 		}
 
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: 	arg.ToAccountID,
-			Amount: 	arg.Amount,
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
 		})
 		if err != nil {
 			return err
@@ -94,7 +94,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 
 		return nil
 	})
-	
+
 	return result, err
 }
 
@@ -107,7 +107,7 @@ func addMoney(
 	amount2 int64,
 ) (account1 Account, account2 Account, err error) {
 	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-		ID: 	accountID1,
+		ID:     accountID1,
 		Amount: amount1,
 	})
 	if err != nil {
@@ -115,7 +115,7 @@ func addMoney(
 	}
 
 	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-		ID: 	accountID2,
+		ID:     accountID2,
 		Amount: amount2,
 	})
 	return
