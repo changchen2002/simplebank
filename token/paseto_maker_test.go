2e9cb0f17b4891e56d05c7a13b2f272f7f90511d
changchen2002 <changchen2002@outlook.com>
2025-03-24 12:41:08 -0700

    create and verify JWT & PASETO token

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,65 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/util"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssueAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidPASETOToken(t *testing.T) {
+	// Generate a random key for PASETO
+	symmetricKey := util.RandomString(32)
+	maker, err := NewPasetoMaker(symmetricKey)
+	require.NoError(t, err)
+
+	// Create a fake (invalid) token manually
+	invalidToken := "v2.local.this_is_not_a_valid_paseto_token"
+
+	// Try to verify the invalid token
+	verifiedPayload, err := maker.VerifyToken(invalidToken)
+
+	// Expect an error and ensure payload is nil
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, verifiedPayload)
+}
